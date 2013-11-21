548da9a51fd6c3ae11b36b8a5c2fda1c92a0f950
kait <kait@kaittop.(none)>
2013-11-21 21:07:09 +0100

    Exp part 2

--- frontend/app/js/controllers.js
+++ frontend/app/js/controllers.js
@@ -1,12 +1,19 @@
 var gameApp = angular.module('gameApp', ['ngResource']);
 
 gameApp.controller('PuzzleInstanceCtrl', function PuzzleInstanceCtrl($scope) {
+    /*
+    if (!('query' in $scope)) {
+        $scope.query = 'omg';
+    }
+    */
     $scope.puzzles = [
     {
         'title' : 'n po k',
+        'content' : '3 po 12',
     },
     {
         'title' : 'n po k2',
+        'content' : '8 po 4',
     }
-    ]
+    ];
 });
